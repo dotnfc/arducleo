2109ed197aca5539ed9aafa614cb9a13de60e345
dotnfc <dotnfc@gmail.com>
2016-08-22 08:53:07 +0800

    [arducleo-samples] rename contra file. improve welcome sample. there is a known bug, if turn on the color leds, buz will not work.

--- scripts/samples/welcome/welcome_main.cpp
+++ scripts/samples/welcome/welcome_main.cpp
@@ -13,7 +13,7 @@ PwmOut ledR(PC_6);
 PwmOut ledG(PC_8);
 PwmOut ledB(PC_9);
 
-Serial pc(SERIAL_TX, SERIAL_RX);
+//Serial pc(SERIAL_TX, SERIAL_RX);
 
 /*==============================================================================
  * \brief arduino like 'tone ()'
@@ -24,8 +24,7 @@ void tone(int frequency, float duration)
 
     // calculate the period of the note in turn play
     period = 1000000 / frequency;
-    // Activate the LED of the core to indicate that we are playing a note
-    led = 1;
+
     // set the period calculated at the PWM output
     buz.period_us (period);
     
@@ -33,8 +32,7 @@ void tone(int frequency, float duration)
     buz.write (0.50f);
     // hope the time set by the tone i
     wait_ms (duration);
-    // disabled the core led to indicate the end of the note
-    led = 0;
+
     // ended the note
     buz = 0;
 }
@@ -43,7 +41,7 @@ void tone(int frequency, float duration)
 void analogWrite(mbed::PwmOut *prt, int spd) // write to PWM
 {
     float a = spd / 256.0; // convert 0..256 to '%'
-    pc.printf ( "a - %d, (%f)\n", spd, a );
+    //pc.printf ( "a - %d, (%f)\n", spd, a );
     *prt = a;
 }
 
@@ -55,9 +53,6 @@ void color (unsigned char red, unsigned char green, unsigned char blue)
      analogWrite(&ledR, 255-red);
      analogWrite(&ledB, 255-blue);
      analogWrite(&ledG, 255-green);
-     //ledR = 255-red;
-     //ledB = 255-blue;
-     //ledG = 255-green;
 }	 
 
 void delay (int ms)
@@ -72,13 +67,19 @@ void delay (int ms)
 void loop();
 int main ()
 {
-    pc.baud (115200);
+    __HAL_AFIO_REMAP_SWJ_NOJTAG ();
+    //pc.baud (115200);
     
-    //tone (1000, 400);
-    //tone (2000, 400);
-    ledR = ledG = ledB = 255;
+    tone (1000, 400);
+    tone (600, 400);
+    
+    ledR.period_us (1000000/500);
+    ledG.period_us (1000000/500);
+    ledB.period_us (1000000/500);
+    ledR = ledG = ledB = 1;
     
     while(1) {
+        led = !led;
         loop ();
 #if 0
         led = 1;        
@@ -117,11 +118,11 @@ void loop1 () 	 // run over and over again
      //wait_ms (1000);	//delay for 1 second  
 }
 
-#define FADESPEED 5     // make this higher to slow down
+#define FADESPEED 8     // make this higher to slow down
 void loop() 
 {
   int r, g, b;
- 
+
   // fade from blue to violet
   for (r = 0; r < 256; r++) { 
     analogWrite(&ledR, r);
